22c0e82c72c4adb05de348513db61d6121c6de87
LudovicoPellecchia <ludovico.pellecchia@gmail.com>
2023-09-25 16:01:18 +0200

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,9 @@
+@extends("layouts.mainLayout")
+
+@section("title", "Laravel-Comics - Homepage")
+
+@section("content")
+<div class="container">
+    <h1 class="text-center">Laravel-Comics</h1>
+</div>
+@endsection
